d9ae5a7411c56a973a42d1f277b5bb8eccafb451
malyvsen <5940672+malyvsen@users.noreply.github.com>
2020-05-01 11:38:26 +0200

    javascript demo

---
+++ main.js
@@ -0,0 +1,99 @@
+imagePicker = document.getElementById('select-image');
+originalImage = document.getElementById('original-image');
+transformedImage = document.getElementById('transformed-image');
+angle = Math.PI * .25; // todo: expose to the user
+
+function pickFile() {
+    imagePicker.click();
+}
+
+function usePickedFile() {
+    fileReader = new FileReader();
+    fileReader.onload = function(event) {
+        useImage(event.target.result);
+    };
+    fileReader.readAsDataURL(imagePicker.files[0]);
+}
+
+function useDemoImage() {
+    useImage('experiments/original.jpg');
+}
+
+function useImage(src) {
+    originalImage.onload = function() {
+        let imageData = imageToArray(originalImage);
+        let transformedData = transformImage(imageData);
+        let canvas = document.createElement('canvas');
+        let ctx = canvas.getContext('2d');
+        canvas.width = originalImage.width;
+        canvas.height = originalImage.height;
+        let idata = ctx.createImageData(originalImage.width, originalImage.height);
+        idata.data.set(transformedData);
+        ctx.putImageData(idata, 0, 0);
+        transformedImage.src = canvas.toDataURL();
+    };
+    originalImage.src = src;
+}
+
+function imageToArray(image) {
+    let canvas = document.createElement('canvas');
+    canvas.width = image.width;
+    canvas.height = image.height;
+    let context = canvas.getContext('2d');
+    context.drawImage(image, 0, 0, image.width, image.height);
+    let imageData = context.getImageData(0, 0, image.width, image.height);
+    return imageData.data;
+}
+
+function transformImage(data) {
+    let r = [];
+    let g = [];
+    let b = [];
+    let a = [];
+    for (let idx = 0; idx < data.length; idx += 4) {
+        r.push(data[idx]);
+        g.push(data[idx + 1]);
+        b.push(data[idx + 2]);
+        a.push(data[idx + 3]);
+    }
+    
+    let newR = transformGrayscale(r);
+    let newG = transformGrayscale(g);
+    let newB = transformGrayscale(b);
+    let newA = a;
+
+    let transformed = [];
+    for (let idx = 0; idx < newR.length; idx++) {
+        transformed.push(newR[idx]);
+        transformed.push(newG[idx]);
+        transformed.push(newB[idx]);
+        transformed.push(newA[idx]);
+    }
+    return transformed;
+}
+
+function transformGrayscale(data) {
+    let fft = realFft(data); // todo: this can only deal with power-of-two sized images
+    let transformedFft = fft.map(transformValue);
+    let transformed = inverseRealFft(transformedFft);
+    return transformed;
+}
+
+function transformValue(value) {
+    return new Fourier.Complex(Math.cos(angle), Math.sin(angle)).times(value);
+}
+
+function realFft(data) {
+    let result = [];
+    Fourier.transform(data, result);
+    return result.slice(0, Math.floor(result.length / 2) + 1);
+}
+
+function inverseRealFft(data) {
+    let negativeData = data.slice(1, -1).map((c) => new Fourier.Complex(c.real, -c.imag));
+    negativeData.reverse();
+    let dataWithNegative = data.concat(negativeData);
+    let result = [];
+    Fourier.invert(dataWithNegative, result);
+    return result;
+}
